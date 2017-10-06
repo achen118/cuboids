4149118a0e7133b18fd69ee23bc13803d0fdf0c0
Alice Chen <alicechen118@gmail.com>
2017-10-05 23:02:00 -0700

    Add coding challenge files

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -3,15 +3,70 @@ class Cuboid
   
   #BEGIN public methods that should be your starting point
 
+  def initialize(origin, length, height, width)
+    @origin = origin
+    @length = length
+    @height = height
+    @width = width
+  end
+
   def move_to!(x, y, z)
+    @origin[:x], @origin[:y], @origin[:z] = x, y, z
+    return @origin
   end
   
   def vertices
+    vert_arr = []
+    vert_arr << [self.x1, self.y1, self.z1]
+    vert_arr << [self.x2, self.y1, self.z1]
+    vert_arr << [self.x1, self.y2, self.z1]
+    vert_arr << [self.x1, self.y1, self.z2]
+    vert_arr << [self.x1, self.y2, self.z2]
+    vert_arr << [self.x2, self.y2, self.z1]
+    vert_arr << [self.x2, self.y1, self.z2]
+    vert_arr << [self.x2, self.y2, self.z2]
+    return vert_arr
   end
   
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
+    if !(self.x2 < other.x1) &&
+      !(other.x2 < self.x1) &&
+      !(self.y2 < other.y1) &&
+      !(other.y2 < self.y1) &&
+      !(self.z2 < other.z1) &&
+      !(other.z2 < self.z1)
+      return true
+    else
+      return false
+    end
   end
 
-  #END public methods that should be your starting point  
+  #END public methods that should be your starting point 
+  
+  protected
+
+  def x1
+    @origin[:x]
+  end
+
+  def x2
+    @origin[:x] + @length
+  end
+
+  def y1
+    @origin[:y]
+  end
+
+  def y2
+    @origin[:y] + @height
+  end
+
+  def z1
+    @origin[:z]
+  end
+
+  def z2
+    @origin[:z] + @width
+  end
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -4,14 +4,51 @@ require 'cuboid'
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
+  
   describe "move_to" do
+    subject { Cuboid.new({x: 1, y: 2, z: 3}, 5, 3, 4) }
     it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+      expect(subject.move_to!(1,2,3)).to eq({x: 1, y: 2, z: 3})
     end
   end    
   
+  subject(:cuboid1) { Cuboid.new({x: 1, y: 2, z: 3}, 5, 3, 4) }
+  subject(:cuboid2) { Cuboid.new({x: 5, y: 4, z: 5}, 3, 6, 4) }
+  subject(:cuboid3) { Cuboid.new({x: 1, y: 2, z: 3}, 5, 3, 4) }
+  subject(:cuboid4) { Cuboid.new({x: 2, y: 3, z: 4}, 1, 1, 1) }
+  subject(:cuboid5) { Cuboid.new({x: 6, y: 2, z: 3}, 5, 3, 4) }
+  subject(:cuboid6) { Cuboid.new({x: 20, y: 30, z: 20}, 5, 3, 4) }
+
   describe "intersects?" do
+    it "returns true if the two cuboids intersect each other" do
+      expect(cuboid1.intersects?(cuboid2)).to be true
+    end
+    it "returns true if two equal cuboids are directly on top of each other" do
+      expect(cuboid1.intersects?(cuboid3)).to be true
+    end
+    it "returns true if one cuboid is inside the other" do
+      expect(cuboid1.intersects?(cuboid4)).to be true
+    end
+    it "returns true if the two cuboids share an axis" do
+      expect(cuboid1.intersects?(cuboid5)).to be true
+    end
+    it "returns false if the two cuboids do not intersect each other" do
+      expect(cuboid1.intersects?(cuboid6)).to be false
+    end
   end
 
+  describe "vertices" do
+    it "returns the 8 vertices of a cuboid" do
+      expect(cuboid1.vertices).to eq([
+        [1, 2, 3],
+        [6, 2, 3],
+        [1, 5, 3],
+        [1, 2, 7],
+        [1, 5, 7],
+        [6, 5, 3],
+        [6, 2, 7],
+        [6, 5, 7]
+      ])
+    end
+  end
 end
